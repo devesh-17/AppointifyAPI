e455ce6980b159d8b0482bee0a5fe485a101e324
dhruv.jain <it.dhruvjain@gmail.com>
2024-08-25 22:44:11 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CityConnect.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
